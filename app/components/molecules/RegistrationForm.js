798654227bbd5238a0dfee45bc1c7c1b0e8c1a4d
RichardSimarmata <47414726+RichardSimarmata@users.noreply.github.com>
2021-10-29 19:52:05 +0700

    mantap
    mantap dah

---
+++ app/components/molecules/RegistrationForm.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { View } from 'react-native'
+import InputFieldText from '../atom/InputFieldText'
+
+const CreateRegistrationForm = () => {
+
+    return (
+        <View>
+            <InputFieldText textLabel='Username' textPlaceholder='Username'  />
+            <InputFieldText textLabel='Password' textPlaceholder='Password' />
+        </View>
+    )
+}
+
+export default CreateRegistrationForm
